23b6c178ef93379c2e8ca047d40806f25ba9171e
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-04-26 17:01:35 +0200

    Finished Roll a Ball

--- Roll A Ball/Assets/Scripts/PlayerControler.cs
+++ Roll A Ball/Assets/Scripts/PlayerControler.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControler : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+		SetCountText();
+		winText.text = "";
     }
 
     void FixedUpdate()
@@ -27,6 +35,16 @@ public class PlayerControler : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+		if (count >= 12){
+			winText.text = "You win!";
+		}
     }
 }
